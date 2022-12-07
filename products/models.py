e5555431d35ede5a4c4d375f1637ceac440f3e91
akshit-jain01 <akshitjain.1112003@gmail.com>
2022-12-07 15:50:42 +0530

    added procfile

--- products/models.py
+++ products/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
 class Category(models.Model):
     category = models.CharField(max_length=255)
+    # product = models.ManyToManyField(Products, max_length=255)
 
     def __str__(self):
         return self.category
 
 class Products(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4())
     product = models.CharField(max_length=255)
     category = models.ManyToManyField(Category, related_name="catergory")
     price = models.IntegerField()
