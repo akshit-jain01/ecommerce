4372e2d333b98e45394982a28e6806538bcabb73
akshit-jain01 <akshitjain.1112003@gmail.com>
2022-12-07 16:10:08 +0530

    hidden secret key

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&#3&fj@(1x#o++1_1z2@w$ys7hz^dh%+icwqr&_fp&j2t80k#-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
