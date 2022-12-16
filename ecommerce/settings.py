f360f8bd76d684892b9caf0c780c68138cdb7ae1
akshit-jain01 <akshitjain.1112003@gmail.com>
2022-12-16 22:37:13 +0530

    updated pip

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -95,6 +95,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'railway',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'XV5gkaBlCjNHlsPDbOwe',
+        # 'HOST': 'containers-us-west-151.railway.app',
+        # 'PORT': '7903',
     }
 }
 
