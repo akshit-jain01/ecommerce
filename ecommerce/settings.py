92d43ae3aa2d50dd75c2154a07c4364b11e8e345
akshit-jain01 <91596978+akshit-jain01@users.noreply.github.com>
2022-12-17 16:31:41 +0530

    updated allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','picky-production-a351.up.railway.app']
+# ALLOWED_HOSTS = ['localhost','picky-production-a351.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 # PYTHON_VERSION = config('PYTHON_VERSION')
 
