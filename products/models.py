a871b9988a3675fbd609f4e3a4c49e04c6cfe133
akshit-jain01 <akshitjain.1112003@gmail.com>
2023-01-01 12:24:26 +0530

    added product model

--- products/models.py
+++ products/models.py
@@ -1,21 +1,30 @@
 from django.db import models
 import uuid
 
+from django.utils.text import slugify
 
-# Create your models here.
+class ColorVariant(models.Model):
+    color = models.CharField()
+    color_code = models.CharField()
 class Category(models.Model):
-    category = models.CharField(max_length=255)
-    # product = models.ManyToManyField(Products, max_length=255)
+    category_name = models.CharField(max_length=255)
+    slug = models.SlugField()
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.category_name)
+    
     def __str__(self):
-        return self.category
+        return self.category_name
 
 class Products(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4())
-    product = models.CharField(max_length=255)
-    category = models.ManyToManyField(Category, related_name="catergory")
+    product_name = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.IntegerField()
+    # image = models.ImageField()
 
     def __str__(self):
-        return self.product
+        return self.product_name
+
 
